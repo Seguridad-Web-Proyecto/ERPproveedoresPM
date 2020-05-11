f1a72e6ecea8498f7e3134d7f63a864df8cd5cbd
dannsntg <50391430+dannsntg@users.noreply.github.com>
2020-05-11 13:40:32 -0500

    JAAS V1

---
+++ src/main/java/entidades/login.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+import dao.UsuarioJpaController;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.security.Principal;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.transaction.UserTransaction;
+import jsf.clases.util.JsfUtil;
+
+/**
+ *
+ * @author jaker
+ */
+@Named(value = "login")
+@SessionScoped
+public class login implements Serializable
+{
+
+    private String usuario, contrasenia;
+    private String errorMessage;
+    private int errorsCounter;
+
+    public String getUsuario()
+    {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario)
+    {
+        this.usuario = usuario;
+    }
+
+    public String getContrasenia()
+    {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia)
+    {
+        this.contrasenia = contrasenia;
+    }
+
+    
+
+    public String login3()
+    {
+        FacesContext context = FacesContext.getCurrentInstance();
+        HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
+        try
+        {
+            request.login(usuario, contrasenia);
+        } catch (ServletException ex)
+        {
+            System.out.println(ex.getMessage());
+            if (ex.getMessage().contains("Login failed"))
+            {
+                errorMessage = "login.failed";
+            }
+            return "/faces/login.xhtml";
+        }
+        Principal user = request.getUserPrincipal();
+//    setPersons(new UsersJpaController(utx,emf).findUsers(usuario));
+//    context.getExternalContext().getSessionMap().put("persons", persons);
+
+//        if (request.isUserInRole("ADMINS"))
+//        {
+//            return "/secured/admin/menu.xhtml";
+//        } else
+//        {
+            return "/secured/user/menu.xhtml";
+//        }
+    }
+    
+    public boolean isRolCompras()
+    {
+        FacesContext context = FacesContext.getCurrentInstance();
+        HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
+        
+        return request.isUserInRole("COMPRAS");
+    }
+
+    public String logout()
+    {
+        FacesContext context = FacesContext.getCurrentInstance();
+        ExternalContext externalContext = context.getExternalContext();
+        HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
+        try
+        {
+            externalContext.invalidateSession();
+            request.logout();
+        } catch (ServletException ex)
+        {
+            Logger.getLogger(login.class.getName()).log(Level.SEVERE, null, ex);
+            return "error";
+        }
+
+        errorMessage = "";
+        errorsCounter = 0;
+        return "/login.xhtml?faces-redirect=true";
+    }
+
+}
--- src/main/java/jsf/clases/UsuarioController.java
+++ src/main/java/jsf/clases/UsuarioController.java
@@ -4,8 +4,12 @@ import entidades.Usuario;
 import jsf.clases.util.JsfUtil;
 import jsf.clases.util.JsfUtil.PersistAction;
 import beans.sessions.UsuarioFacade;
+import entidades.Encoding;
+import entidades.Rol;
+import java.io.IOException;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
@@ -21,112 +25,190 @@ import javax.faces.convert.FacesConverter;
 
 @Named("usuarioController")
 @SessionScoped
-public class UsuarioController implements Serializable {
+public class UsuarioController implements Serializable
+{
 
     @EJB
     private beans.sessions.UsuarioFacade ejbFacade;
     private List<Usuario> items = null;
     private Usuario selected;
+    private String pass2;
+    
 
-    public UsuarioController() {
+    public UsuarioController()
+    {
     }
 
-    public Usuario getSelected() {
+    public Usuario getSelected()
+    {
         return selected;
     }
 
-    public void setSelected(Usuario selected) {
+    public void setSelected(Usuario selected)
+    {
         this.selected = selected;
     }
 
-    protected void setEmbeddableKeys() {
+    public String getPass2()
+    {
+        return pass2;
     }
 
-    protected void initializeEmbeddableKey() {
+    public void setPass2(String pass2)
+    {
+        this.pass2 = pass2;
+    }
+    
+    protected void setEmbeddableKeys()
+    {
     }
 
-    private UsuarioFacade getFacade() {
+    protected void initializeEmbeddableKey()
+    {
+    }
+
+    private UsuarioFacade getFacade()
+    {
         return ejbFacade;
     }
 
-    public Usuario prepareCreate() {
+    public String registrar() throws IOException
+    {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        selected.setActivo(true);
+        selected.setFechaCreacion(new Date());
+
+        if (pass2.equals(selected.getContrasenia()))
+        {
+            selected.setContrasenia(Encoding.convertPass(selected.getContrasenia()));
+            selected.setRol(new Rol(selected.getEmail(), "USERS"));
+
+            if (getUsuario(selected.getEmail()) != null)
+            {
+                JsfUtil.addErrorMessage("El usuario ya existe");
+                return null;
+            }
+
+            create();
+            if (!JsfUtil.isValidationFailed())
+            {
+                JsfUtil.addSuccessMessage("Ok");
+                return "regdone.xhtml";
+
+            } else
+            {
+                JsfUtil.addErrorMessage("Ocurrió un error");
+                return null;
+            }
+        } else
+        {
+            JsfUtil.addErrorMessage("Las contraseñas no coinciden");
+            return null;
+        }
+    }
+
+    public Usuario prepareCreate()
+    {
         selected = new Usuario();
         initializeEmbeddableKey();
         return selected;
     }
 
-    public void create() {
+    public void create()
+    {
         persist(PersistAction.CREATE, ResourceBundle.getBundle("/Bundle").getString("UsuarioCreated"));
-        if (!JsfUtil.isValidationFailed()) {
+        if (!JsfUtil.isValidationFailed())
+        {
             items = null;    // Invalidate list of items to trigger re-query.
         }
     }
 
-    public void update() {
+    public void update()
+    {
         persist(PersistAction.UPDATE, ResourceBundle.getBundle("/Bundle").getString("UsuarioUpdated"));
     }
 
-    public void destroy() {
+    public void destroy()
+    {
         persist(PersistAction.DELETE, ResourceBundle.getBundle("/Bundle").getString("UsuarioDeleted"));
-        if (!JsfUtil.isValidationFailed()) {
+        if (!JsfUtil.isValidationFailed())
+        {
             selected = null; // Remove selection
             items = null;    // Invalidate list of items to trigger re-query.
         }
     }
 
-    public List<Usuario> getItems() {
-        if (items == null) {
+    public List<Usuario> getItems()
+    {
+        if (items == null)
+        {
             items = getFacade().findAll();
         }
         return items;
     }
 
-    private void persist(PersistAction persistAction, String successMessage) {
-        if (selected != null) {
+    private void persist(PersistAction persistAction, String successMessage)
+    {
+        if (selected != null)
+        {
             setEmbeddableKeys();
-            try {
-                if (persistAction != PersistAction.DELETE) {
+            try
+            {
+                if (persistAction != PersistAction.DELETE)
+                {
                     getFacade().edit(selected);
-                } else {
+                } else
+                {
                     getFacade().remove(selected);
                 }
                 JsfUtil.addSuccessMessage(successMessage);
-            } catch (EJBException ex) {
+            } catch (EJBException ex)
+            {
                 String msg = "";
                 Throwable cause = ex.getCause();
-                if (cause != null) {
+                if (cause != null)
+                {
                     msg = cause.getLocalizedMessage();
                 }
-                if (msg.length() > 0) {
+                if (msg.length() > 0)
+                {
                     JsfUtil.addErrorMessage(msg);
-                } else {
+                } else
+                {
                     JsfUtil.addErrorMessage(ex, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
                 }
-            } catch (Exception ex) {
+            } catch (Exception ex)
+            {
                 Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
                 JsfUtil.addErrorMessage(ex, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
             }
         }
     }
 
-    public Usuario getUsuario(java.lang.String id) {
+    public Usuario getUsuario(java.lang.String id)
+    {
         return getFacade().find(id);
     }
 
-    public List<Usuario> getItemsAvailableSelectMany() {
+    public List<Usuario> getItemsAvailableSelectMany()
+    {
         return getFacade().findAll();
     }
 
-    public List<Usuario> getItemsAvailableSelectOne() {
+    public List<Usuario> getItemsAvailableSelectOne()
+    {
         return getFacade().findAll();
     }
 
     @FacesConverter(forClass = Usuario.class)
-    public static class UsuarioControllerConverter implements Converter {
+    public static class UsuarioControllerConverter implements Converter
+    {
 
         @Override
-        public Object getAsObject(FacesContext facesContext, UIComponent component, String value) {
-            if (value == null || value.length() == 0) {
+        public Object getAsObject(FacesContext facesContext, UIComponent component, String value)
+        {
+            if (value == null || value.length() == 0)
+            {
                 return null;
             }
             UsuarioController controller = (UsuarioController) facesContext.getApplication().getELResolver().
@@ -134,28 +216,37 @@ public class UsuarioController implements Serializable {
             return controller.getUsuario(getKey(value));
         }
 
-        java.lang.String getKey(String value) {
+        java.lang.String getKey(String value)
+        {
             java.lang.String key;
             key = value;
             return key;
         }
 
-        String getStringKey(java.lang.String value) {
+        String getStringKey(java.lang.String value)
+        {
             StringBuilder sb = new StringBuilder();
             sb.append(value);
             return sb.toString();
         }
 
         @Override
-        public String getAsString(FacesContext facesContext, UIComponent component, Object object) {
-            if (object == null) {
+        public String getAsString(FacesContext facesContext, UIComponent component, Object object)
+        {
+            if (object == null)
+            {
                 return null;
             }
-            if (object instanceof Usuario) {
+            if (object instanceof Usuario)
+            {
                 Usuario o = (Usuario) object;
                 return getStringKey(o.getEmail());
-            } else {
-                Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, "object {0} is of type {1}; expected type: {2}", new Object[]{object, object.getClass().getName(), Usuario.class.getName()});
+            } else
+            {
+                Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, "object {0} is of type {1}; expected type: {2}", new Object[]
+                {
+                    object, object.getClass().getName(), Usuario.class.getName()
+                });
                 return null;
             }
         }
